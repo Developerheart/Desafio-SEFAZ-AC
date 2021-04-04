3b68767889be01478d04f86a2324bc906c60da87
Developerheart <62905562+Developerheart@users.noreply.github.com>
2021-04-04 13:37:34 -0300

    Initial commit

---
+++ src/main/java/br/gov/ac/sefaz/treinamento/financas/HelloApplication.java
@@ -0,0 +1,9 @@
+package br.gov.ac.sefaz.treinamento.financas;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/br/gov/ac/sefaz/treinamento/financas/entities/LancamentoFinanceiro.java
@@ -0,0 +1,10 @@
+package br.gov.ac.sefaz.treinamento.financas.entities;
+
+import java.io.Serializable;
+
+public class LancamentoFinanceiro implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+
+}
